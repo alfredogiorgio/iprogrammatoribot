4784462533dbdfe371482099e3dc74552edf2ed3
CodeSandbox <support@codesandbox.io>
2023-11-16 14:47:25 +0000

    Sandbox snapshot commit

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import requests
-r = requests.get('https://www.iprogrammatori.it/rss/offerte-di-lavoro.xml')
+from bs4 import BeautifulSoup
 
-print(r.status_code)
+headers = {
+    'User-Agent': 'My User Agent 1.0',
+    'From': 'youremail@domain.example'  # This is another valid field
+}
+r = requests.get(
+    'https://www.iprogrammatori.it/rss/offerte-di-lavoro.xml', headers=headers)
+
+print(r.text)
