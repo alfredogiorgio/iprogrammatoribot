c6480b464d21f288f3f2eb7b4f16301f6565c219
CodeSandbox <support@codesandbox.io>
2023-11-16 15:46:36 +0000

    Sandbox snapshot commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import codecs
 
 headers = {
     'User-Agent': 'My User Agent 1.0',
@@ -8,4 +9,16 @@ headers = {
 r = requests.get(
     'https://www.iprogrammatori.it/rss/offerte-di-lavoro.xml', headers=headers)
 
-print(r.text)
+soup = BeautifulSoup(r.text, 'lxml-xml')
+
+channel = soup.find('channel')
+items = channel.find_all('item')
+
+print(items[0].find('title').text)
+print(items[0].find('link').text)
+print(items[0].find('category').text)
+print(items[0].find('pubDate').text)
+
+description = BeautifulSoup(items[0].find('description').text, 'html.parser')
+
+print(codecs.decode(description.get_text(), 'utf-8'))
