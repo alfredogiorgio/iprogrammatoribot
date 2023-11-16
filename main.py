e77fab625c8840ad0ed4c9a06952033eec697087
CodeSandbox <support@codesandbox.io>
2023-11-16 20:46:14 +0000

    Sandbox snapshot commit

--- main.py
+++ main.py
@@ -1,69 +1,71 @@
-import requests
 from bs4 import BeautifulSoup
 import json
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-
-headers = {
-    'User-Agent': 'My User Agent 1.0'
-}
-r = requests.get(
-    'https://www.iprogrammatori.it/rss/offerte-lavoro-crawler.xml', headers=headers)
-
-soup = BeautifulSoup(r.text, 'lxml-xml')
+import aiohttp
 
 
-jobs = soup.find_all('job')
+async def scrape():
 
-f = open('jobs.json')
-data = json.load(f)
+    print("andato")
+    headers = {
+        'User-Agent': 'My User Agent 1.0'
+    }
 
-size = len(data)
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://www.iprogrammatori.it/rss/offerte-lavoro-crawler.xml', headers=headers) as resp:
+            r = await resp.text()
 
-for job in jobs:
-    jobJson = {}
+    soup = BeautifulSoup(r.text, 'lxml-xml')
 
-    jobJson['id'] = job.find('id').text
-    jobJson['title'] = job.find('title').text
-    jobJson['url'] = job.find('url').text
-    jobJson['content'] = job.find('content').text.encode(
-        'iso-8859-1').decode('utf-8', errors='ignore')
+    jobs = soup.find_all('job')
 
-    try:
-        jobJson['city'] = job.find('city').text
-    except:
-        jobJson['city'] = "Non specificato"
+    f = open('jobs.json')
+    data = json.load(f)
 
-    try:
-        jobJson['company'] = job.find('company').text
-    except:
-        jobJson['company'] = "Non specificato"
+    size = len(data)
 
-    try:
-        jobJson['requirements'] = job.find('requirements').text
-    except:
-        jobJson['requirements'] = "Non specificato"
+    for job in jobs:
+        jobJson = {}
 
-    try:
-        jobJson['date'] = job.find('date').text.encode(
+        jobJson['id'] = job.find('id').text
+        jobJson['title'] = job.find('title').text
+        jobJson['url'] = job.find('url').text
+        jobJson['content'] = job.find('content').text.encode(
             'iso-8859-1').decode('utf-8', errors='ignore')
-    except:
-        jobJson['date'] = "Non specificato"
 
-    try:
-        jobJson['jobtype'] = job.find('jobtype').text
-    except:
-        jobJson['jobtype'] = "Non specificato"
+        try:
+            jobJson['city'] = job.find('city').text
+        except:
+            jobJson['city'] = "Non specificato"
 
-    data.append(jobJson)
+        try:
+            jobJson['company'] = job.find('company').text
+        except:
+            jobJson['company'] = "Non specificato"
 
-    print(jobJson['title'])
+        try:
+            jobJson['requirements'] = job.find('requirements').text
+        except:
+            jobJson['requirements'] = "Non specificato"
 
-with open('jobs.json', 'w') as file:
-    json.dump(data, file, indent=4)
+        try:
+            jobJson['date'] = job.find('date').text.encode(
+                'iso-8859-1').decode('utf-8', errors='ignore')
+        except:
+            jobJson['date'] = "Non specificato"
+
+        try:
+            jobJson['jobtype'] = job.find('jobtype').text
+        except:
+            jobJson['jobtype'] = "Non specificato"
+
+        data.append(jobJson)
+
+        print(jobJson['title'])
+
+    with open('jobs.json', 'w') as file:
+        json.dump(data, file, indent=4)
 
 sched = AsyncIOScheduler()
-
-# Esegue il lavoro ogni giorno alle 5:30pm o 17:30
-sched.add_job(job_function, 'cron', day_of_week='mon-fri', hour=17, minute=30)
-
+sched.add_job(scrape, 'interval', minutes=1)
 sched.start()
